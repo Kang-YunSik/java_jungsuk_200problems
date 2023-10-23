4567eed5128acb74aed03f6ec08e70c89ec4931a
kys <kang_ys_1@naver.com>
2023-10-23 16:08:31 +0900

    클래스 메소드 선언

--- Ch6_Object-oriented-Programming/Exercise6_4.java
+++ Ch6_Object-oriented-Programming/Exercise6_4.java
@@ -2,10 +2,13 @@
 
 class Exercise6_4{
     public static void main(String[] args) {
+        // 객체 s 생성
         Student2 s = new Student2();
+        
+        // s의 필드 값 정의
         s.name = "홍길동";
         s.ban = 1;
-        s.no = 1;
+        s.no = 2;
         s.kor = 100;
         s.eng = 60;
         s.math = 76;
@@ -15,7 +18,11 @@ class Exercise6_4{
         System.out.println("평균:"+s.getAverage());
     }
 }
+
+// 클래스 정의
 class Student2 {
+    
+    // 필드 선언
     String name; // 학생이름
     int ban; // 반
     int no; // 번호
@@ -23,10 +30,13 @@ class Student2 {
     int eng; // 영어점수
     int math; // 수학점수
 
+    // 함수 선언
+    // 함수를 선언할 때 리턴의 데이터 타입을 맞춰야 한다.
     int getTotal(){
         return kor+eng+math;
     }
 
+    // 함수 선언
     float getAverage(){
         return (int)getTotal()/3f * 10 + 0.5f;
     }
