7d5eda6383566cd5ff46c8ed1e2547693ca96060
kys <kang_ys_1@naver.com>
2023-10-30 19:54:11 +0900

    이전 채널로 이동하는 기능 추가

--- Ch7_Object-orented-Programming2/Exercise7_10.java
+++ Ch7_Object-orented-Programming2/Exercise7_10.java
@@ -1,25 +1,37 @@
 // MvTv2 클래스의 멤버변수 isPowerOn, channel, volume을 클래스 외부에서 접근할 수 없도록 제어자를 붙이고,
 // 대신 이 멤버변수들의 값을 어디서나 읽고 변경할 수 있도록 getter와 setter메서드를 추가하라.
 
+// 이전 채널로 이동하는 기능을 추가
+// 메서드명: gotoPreVChannel
+// 기   능: 현재 채널을 이전 채널로 변경한다.
+// 반환타입: 없음
+// 매개변수: 없음
+
 public class Exercise7_10 {
     public static void main(String[] args) {
-        MvTv2 t = new MvTv2();
+        MyTv2 t = new MyTv2();
 
         // setter 호출:
         t.setChannel(10);
+        System.out.println("CH: "+t.getChannel()); // CH:10
+        t.setChannel(20);
+        System.out.println("CH: "+t.getChannel()); // CH:20
+        // 이전 채널로 변경
+        t.gotoPrevChannel();
         // getter 호출:
-        System.out.println("CH: " + t.getChannel());
+        System.out.println("CH: "+t.getChannel()); // CH:10
 
     }
 }
 
-class MvTv2 {
+class MyTv2 {
 
     // 멤버변수 선언, private 접근 제한자로 클래스 외부에서 접근 불가.
     // Getter, Setter 메소드를 선언하면 접근할 수 있다.
     private boolean isPowerOn;
     private int channel;
     private int volume;
+    private int prevChannel; // 이전채널
 
     final int MAX_VOLUEM = 100 ;
     final int MIN_VOLUEM = 0 ;
@@ -47,10 +59,15 @@ class MvTv2 {
     }
 
     public void setChannel(int channel) {
-        if (volume > MAX_CHANNEL || volume < MIN_CHANNEL)
+        if (channel > MAX_CHANNEL || channel < MIN_CHANNEL)
             return;
 
+        prevChannel = this.channel; // 현재 채널을 이전 채널에 저장 (새로운 channel값으로 변경되기 전에 수행)
         this.channel = channel; // 매개변수 channel을 그대로 반환
+    }
+
+    public void gotoPrevChannel() {
+        setChannel(prevChannel); // 현재 채널을 이전 채널로 변경
     }
 
     public void setVolume(int volume) {
