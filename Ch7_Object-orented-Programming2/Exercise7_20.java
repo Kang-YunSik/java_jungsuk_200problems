5ab1ab0c386d34caa138cd702c1e8ee369266d95
kys <kang_ys_1@naver.com>
2023-10-31 20:28:37 +0900

    객체(참조변수)의 타입에 따른 멤버변수, 메소드 호출

--- Ch7_Object-orented-Programming2/Exercise7_20.java
+++ Ch7_Object-orented-Programming2/Exercise7_20.java
@@ -1,9 +1,9 @@
 // 결과를 예상하시오.
 public class Exercise7_20 {
     public static void main(String[] args) {
-        // Parent2 클래스 객체 p 생성
+        // Parent2 클래스에서 객체 p 생성. Child2를 참조
         Parent2 p = new Child2();
-        // Child2 클래스 객체 c 생성
+        // Child2 클래스에서 객체 c 생성. Child2를 참조
         Child2 c = new Child2();
         // 객체 p,c 는 참조변수로써, 둘 다 Child2 인스턴스를 참조하고 있다.
         // 멤버는 참조변수가 생성된 클래스의 값을 따르지만,
